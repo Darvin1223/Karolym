e57e43823a685611af632f4d37fdd47d974e7e76
Darvin <darvin.rod10@gmail.com>
2020-11-28 01:04:31 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+
+
+// // Jquery
+ $(document).ready(main);
+
+var contador = 1;
+ 
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+ 
+		if(contador == 1){
+			$('nav').animate({
+				'left': '0'
+            });
+            $('nav').css({
+				'max-height':'370px',
+				'width':'18rem'
+			});
+		
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+			
+			
+		}
+ 
+	});
+ 
+};  
+
+
+// DOM LOADEED
+document.addEventListener('DOMContentLoaded',()=> {
+	// let ancho = screen.width;
+	// let altura = screen.height;
+	// console.log(`la altura es: ${altura}`);
+	// console.log(`El ancho es: ${ancho}`);
+	
+	// Contacto - formulario
+	let formulario = document.getElementById("formulario");
+
+	formulario.addEventListener("submit",function(e){
+		e.preventDefault();
+		let Nombre= document.getElementById('Nombre').value,
+		Telefono = document.getElementById('Numero').value,
+		Correo = document.getElementById('Correo').value,
+		Mensaje = document.getElementById('Mensaje').value;
+
+
+		let Datos = {
+            Nombre:` ${Nombre}`,
+            Telefono: `${Telefono}`,
+            Correo: `${Correo}`,
+            Mensaje: `${Mensaje}`
+		};
+				
+		fetch("/formulario", {
+      	method: 'POST',
+      	headers: [ 
+			  ["Content-Type", "application/json"]
+			 ],
+			 credentials: "include",
+      	body: JSON.stringify(Datos),
+			});
+			// .then(data => {
+			// 		return data.JSON();
+			// 	}).then( data => {
+			// 		console.log("klk manito esto es lo que llega del Back ", data);
+			// 	});
+	
+	
+		console.log(Datos);
+	});
+	
+
+	
+});
